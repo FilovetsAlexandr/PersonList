7efd2968b36eb8236f44be2d54e32c5c6849c607
FilovetsAlexandr <sohan.boxan@gmail.com>
2023-09-13 13:54:07 +0300

    Project created

---
+++ PersonsList/PersonsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonsList
+//
+//  Created by Alexandr Filovets on 13.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
