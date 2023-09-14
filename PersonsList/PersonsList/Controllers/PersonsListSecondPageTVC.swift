02af1f41101d364fd7e962f0e48c34e009f818d4
FilovetsAlexandr <sohan.boxan@gmail.com>
2023-09-14 14:29:55 +0300

    project done the best

--- PersonsList/PersonsList/Controllers/PersonsListSecondPageTVC.swift
+++ PersonsList/PersonsList/Controllers/PersonsListSecondPageTVC.swift
@@ -13,19 +13,32 @@ class PersonsListSecondPageTVC: UITableViewController {
         navigationItem.title = "Persons list"
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int { 1 }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { PersonsTVC.persons.count }
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
 
-        let person = PersonsTVC.persons[indexPath.row]
-
-        cell.namePersonLbl.text = person.name
+        let person = PersonsTVC.persons[indexPath.section]
+        
         cell.emailPersonLbl.text = person.email
         cell.phonePersonLbl.text = person.phoneNumber
 
         return cell
     }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return PersonsTVC.persons.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? { PersonsTVC.persons[section].name }
+    
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+            let header = view as! UITableViewHeaderFooterView
+            header.textLabel?.textColor = .white
+            header.backgroundConfiguration?.backgroundColor = .darkGray
+            header.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 17)
+        }
 }
