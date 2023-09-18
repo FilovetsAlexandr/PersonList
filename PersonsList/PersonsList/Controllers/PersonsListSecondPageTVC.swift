351c265dbb5ffaff193720c298127ae18bb21cc6
FilovetsAlexandr <sohan.boxan@gmail.com>
2023-09-19 01:03:01 +0300

    add final + private, +delete, +moving, + opening of detailed information

--- PersonsList/PersonsList/Controllers/PersonsListSecondPageTVC.swift
+++ PersonsList/PersonsList/Controllers/PersonsListSecondPageTVC.swift
@@ -1,13 +1,14 @@
-//
-//  PersonsListSecondPageTVC.swift
-//  PersonsList
-//
-//  Created by Alexandr Filovets on 13.09.23.
-//
-
 import UIKit
 
-class PersonsListSecondPageTVC: UITableViewController {
+final class PersonsListSecondPageTVC: UITableViewController {
+    private var isEditingEnabled = false
+
+    @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
+        isEditingEnabled = !isEditingEnabled
+        tableView.setEditing(isEditingEnabled, animated: true)
+        sender.title = isEditingEnabled ? "Done" : "Edit"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Persons list"
@@ -17,28 +18,23 @@ class PersonsListSecondPageTVC: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
 
         let person = PersonsTVC.persons[indexPath.section]
-        
+
         cell.emailPersonLbl.text = person.email
         cell.phonePersonLbl.text = person.phoneNumber
 
         return cell
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return PersonsTVC.persons.count
-    }
+    override func numberOfSections(in tableView: UITableView) -> Int { PersonsTVC.persons.count }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 1 }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? { PersonsTVC.persons[section].name }
-    
-    
+
     override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-            let header = view as! UITableViewHeaderFooterView
-            header.textLabel?.textColor = .white
-            header.backgroundConfiguration?.backgroundColor = .darkGray
-            header.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 17)
-        }
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.textColor = .white
+        header.backgroundConfiguration?.backgroundColor = .darkGray
+        header.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 17)
+    }
 }
